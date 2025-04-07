d12c2ee6bb36e6572bbb6598a8d31e3af6716aa5
ykisana <ykisana@protonmail.com>
2025-04-07 00:42:44 -0400

    remove secure socket

--- src/components/StatusContainer.tsx
+++ src/components/StatusContainer.tsx
@@ -6,7 +6,7 @@ import StatusTile from "./StatusTile";
 
 export default function StatusContainer() {
   console.log("test");
-  const brokerUrl = "wss://test.mosquitto.org:8080/mqtt";
+  const brokerUrl = "ws://test.mosquitto.org:8080/mqtt";
 
   console.log("test2");
   const TEMP_TOPIC = "plant/temp";
